af22c824c7ffee0fb4f2ff928b0e30726924f211
meldiadri4n <meldiadri4n@gmail.com>
2026-08-04 07:56:58 +0700

    update

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,4 +1,6 @@
-<?xml version="1.0" encoding="UTF-8"?>
+@php
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+@endphp
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>{{ url('/') }}</loc>
@@ -7,12 +9,15 @@
         <priority>1.0</priority>
     </url>
 
-    @foreach ($news as $item)
-    <url>
-        <loc>{{ route('news.show', $item->slug) }}</loc>
-        <lastmod>{{ $item->updated_at->toAtomString() }}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
-    </url>
-    @endforeach
+```
+@foreach ($news as $item)
+<url>
+    <loc>{{ route('news.show', $item->slug) }}</loc>
+    <lastmod>{{ $item->updated_at->toAtomString() }}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+</url>
+@endforeach
+```
+
 </urlset>
