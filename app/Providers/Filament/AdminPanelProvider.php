b8dc5ce97f668a6fd680a362f94631ac0ffed796
Adrian <154045838+meldiadrian@users.noreply.github.com>
2026-08-08 11:25:17 +0700

    Add Login page import to AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -18,6 +18,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use App\Filament\Admin\Pages\Auth\Login;
 
 use Filament\View\PanelsRenderHook;
 use Jeffgreco13\FilamentBreezy\BreezyCore;
