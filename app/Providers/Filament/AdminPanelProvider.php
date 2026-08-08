a789454d9222d912fc0ec2146f9c69875e79790b
Adrian <154045838+meldiadrian@users.noreply.github.com>
2026-08-08 11:13:09 +0700

    Refactor login class reference in AdminPanelProvider
    ->login (Login::class)

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,7 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login(\App\Filament\Admin\Pages\Auth\Login::class)
+            ->login(Login::class)
             ->brandName('PARIWISATA BENGKALIS')
             ->colors([
                 'primary' => Color::Amber,
