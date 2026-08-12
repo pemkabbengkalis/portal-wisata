c7ce6dfeab00a4269a246a386b1669f4ee93ecd0
Adrian <154045838+meldiadrian@users.noreply.github.com>
2026-08-12 20:34:53 +0700

    update

--- app/Filament/Admin/Pages/Login.php
+++ app/Filament/Admin/Pages/Login.php
@@ -6,7 +6,5 @@ use Filament\Auth\Pages\Login as BaseLogin;
 
 class Login extends BaseLogin
 {
-    protected string $view = 'filament.admin.pages.auth.login';
+    protected string $view = 'filament.admin.pages.login';
 }
-
-
